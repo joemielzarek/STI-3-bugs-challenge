a8f9c14b33f45ce660baa848b4fe3150beab13ec
Joe Mielzarek <joseph.mielzarek@gmail.com>
2016-05-02 13:43:17 -0500

    fixed three bugs

--- solo_script.js
+++ solo_script.js
@@ -1,52 +1,57 @@
 // ! ! !
 // Three Bugs
 
-var arrayAtticus = ["Atticus", "2405", "47000", 3];
-var arrayJem = ["Jem", "62347", "63500", 4];
-var arrayBoo = ["Boo", "11435", "54000", 3];
-var arrayScout = ["Scout", "6243", "74750", 5];
+var arrayAtticus = ['Atticus', '2405', '47000', 3];
+var arrayJem = ['Jem', '62347', '63500', 4];
+var arrayBoo = ['Boo', '11435', '54000', 3];
+var arrayScout = ['Scout', '6243', '74750', 5];
 
 var array = [arrayAtticus, arrayJem, arrayBoo, arrayScout];
 
 //Create variables used to write to the DOM
-var newEl, newText, position;
+var newEl;
+var newText;
+var position;
+
 //Capture the position of insertion into the DOM
 position = document.getElementById('content');
 
 //Loop the array, extracting each array and writing information to the DOM
 //Note that the information is not 'clean'
-for(var i = 0; i < array.length; i++){
-	array[i] = calculateSTI(array);
- 	newEl = document.createElement('li');
-	newText = document.createTextNode(array[i]);
-	newEl.appendChild(newText);
-	position.appendChild(newEl);
+for (var i = 0; i < array.length; i++) {
+  array[i] = calculateSTI(array);
+  newEl = document.createElement('li');
+  newText = document.createTextNode(array[i]);
+  newEl.appendChild(newText);
+  position.appendChild(newEl);
 }
 
-function calculateSTI(array){
+function calculateSTI(array) {
   var newArray = [];
 
-  newArray[0] = array[0];
+  //Made Adjustment to below statement to point to unique sub-index of array.
+  newArray[0] = array[i][0];
 
-  var employeeNumber = array[1];
-  var baseSalary = array[2];
-  var reviewScore = array[3];
+  var employeeNumber = array[i][1];
+  var baseSalary = (array[i][2]);
+  var reviewScore = array[i][3];
 
   var bonus = getBaseSTI(reviewScore) + getYearAdjustment(employeeNumber) - getIncomeAdjustment(baseSalary);
-  if(bonus > 0.13){
+  if (bonus > 0.13) {
     bonus = 0.13;
   }
 
-  newArray[1] = bonus;
-  newArray[2] = baseSalary * (1.0 + bonus);
+//Rounded NewArray[2] output
+  newArray[1] = Number(bonus);
+  newArray[2] = Math.round(baseSalary * (1.0 + bonus));
   newArray[3] = baseSalary * bonus;
-  console.log(newArray[0] + " " + newArray[1] + " " + newArray[2] + " " + newArray[3]);
+  console.log(newArray[0] + ' ' + newArray[1] + ' ' + newArray[2] + ' ' + newArray[3]);
   return newArray;
 }
 
-function getBaseSTI(reviewScore){
+function getBaseSTI(reviewScore) {
   var basePercent;
-  switch(reviewScore){
+  switch (reviewScore) {
     case 1:
       basePercent = 0;
       break;
@@ -63,22 +68,25 @@ function getBaseSTI(reviewScore){
       basePercent = 0.10;
       break;
   }
-  return basePercent - 1;
+	// Removed - 1 to ensure output is positive.
+  return basePercent;
 }
 
-function getYearAdjustment(employeeNumber){
+function getYearAdjustment(employeeNumber) {
   var yearAdjustment = 0;
-  if(employeeNumber.length == 4){
+  if (employeeNumber.length == 4) {
     yearAdjustment = 0.05;
   }
+
   return yearAdjustment;
 }
 
-function getIncomeAdjustment(salary){
+function getIncomeAdjustment(salary) {
   var incomeAdjustment = 0;
   salary = parseInt(salary);
-  if(salary > 65000){
+  if (salary > 65000) {
     incomeAdjustment = 0.01;
   }
+
   return incomeAdjustment;
 }
